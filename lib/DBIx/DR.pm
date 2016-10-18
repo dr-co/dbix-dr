583e415a6828e3deb945372b04c58f773e0f7d30
Dmitry E. Oboukhov <unera@debian.org>
2016-10-19 01:01:44 +0300

    Bump version

--- lib/DBIx/DR.pm
+++ lib/DBIx/DR.pm
@@ -7,7 +7,7 @@ use DBIx::DR::Util ();
 use DBIx::DR::PlPlaceHolders;
 
 package DBIx::DR;
-our $VERSION = '0.29';
+our $VERSION = '0.30';
 use base 'DBI';
 use Carp;
 $Carp::Internal{ (__PACKAGE__) } = 1;
